866613166c6c64d55aeb1d77f46c870429a55882
Florian Moser <git@famoser.ch>
2016-07-06 12:56:10 +0200

    initial commit & release

---
+++ Famoser.FrameworkEssentials.View/Commands/IndeterminateProgressDisposable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Famoser.FrameworkEssentials.Services.Interfaces;
+using GalaSoft.MvvmLight.Command;
+
+namespace Famoser.FrameworkEssentials.View.Commands
+{
+    public class IndeterminateProgressDisposable<T>
+    {
+        private readonly RelayCommand _relayCommand;
+        private readonly RelayCommand<T> _genericRelayCommand;
+        private readonly Action<bool> _booleanSetter;
+        private readonly T _progressKey;
+        private readonly IProgressService _progressService;
+
+        public IndeterminateProgressDisposable(RelayCommand command, Action<bool> booleanSetter, T key, IProgressService progressService)
+        {
+            _relayCommand = command;
+            _booleanSetter = booleanSetter;
+            _progressKey = key;
+            _progressService = progressService;
+        }
+
+        public IndeterminateProgressDisposable(RelayCommand<T> command, Action<bool> booleanSetter, T key, IProgressService progressService)
+        {
+            _genericRelayCommand = command;
+            _booleanSetter = booleanSetter;
+            _progressKey = key;
+            _progressService = progressService;
+        }
+
+        public void Start()
+        {
+            _progressService.StartIndeterminateProgress(_progressKey);
+            _booleanSetter(true);
+            _relayCommand?.RaiseCanExecuteChanged();
+            _genericRelayCommand?.RaiseCanExecuteChanged();
+        }
+
+        public void Dispose()
+        {
+            _progressService.StopIndeterminateProgress(_progressKey);
+            _booleanSetter(false);
+            _relayCommand?.RaiseCanExecuteChanged();
+            _genericRelayCommand?.RaiseCanExecuteChanged();
+        }
+    }
+}
