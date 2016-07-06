036a7710c7f37fb70e3cfaa3670a566af73db47a
Florian Moser <git@famoser.ch>
2016-07-06 14:49:58 +0200

    fixed for geenric types

--- Famoser.FrameworkEssentials.View/Commands/IndeterminateProgressDisposable.cs
+++ Famoser.FrameworkEssentials.View/Commands/IndeterminateProgressDisposable.cs
@@ -8,10 +8,10 @@ using GalaSoft.MvvmLight.Command;
 
 namespace Famoser.FrameworkEssentials.View.Commands
 {
-    public class IndeterminateProgressDisposable<T> : IDisposable
+    public class IndeterminateProgressDisposable<T, T2> : IDisposable
     {
         private readonly RelayCommand _relayCommand;
-        private readonly RelayCommand<T> _genericRelayCommand;
+        private readonly RelayCommand<T2> _genericRelayCommand;
 
         private readonly Action<bool> _booleanSetter;
         private readonly T _progressKey;
@@ -25,7 +25,7 @@ namespace Famoser.FrameworkEssentials.View.Commands
             _progressService = progressService;
         }
 
-        public IndeterminateProgressDisposable(RelayCommand<T> command, Action<bool> booleanSetter, T key, IProgressService progressService)
+        public IndeterminateProgressDisposable(RelayCommand<T2> command, Action<bool> booleanSetter, T key, IProgressService progressService)
         {
             _genericRelayCommand = command;
             _booleanSetter = booleanSetter;
