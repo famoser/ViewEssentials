5cd88a239d647cafc498a1fa9e0d00b43977e7ec
Florian Moser <git@famoser.ch>
2016-07-19 13:36:52 +0200

    added unit test

---
+++ Famoser.FrameworkEssentials.Tests/TestLoadingRelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Famoser.FrameworkEssentials.Services;
+using Famoser.FrameworkEssentials.View.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Famoser.FrameworkEssentials.Tests
+{
+    [TestClass]
+    public class TestLoadingRelayCommand
+    {
+        [TestMethod]
+        public void TestDisposable()
+        {
+            var command = new LoadingRelayCommand(() => { });
+            var pgs = new ProgressService();
+            var key = Guid.NewGuid();
+
+            Assert.IsTrue(command.CanExecute(null));
+            using (var g = command.GetProgressDisposable(pgs, key))
+            {
+                Assert.IsFalse(command.CanExecute(null));
+                Assert.IsTrue(pgs.AnyProgressActive);
+                Assert.IsTrue(pgs.IndeterminateProgressActive);
+                Assert.IsTrue(pgs.GetActiveIndeterminateProgresses().Count == 1);
+                Assert.IsTrue((Guid)pgs.GetActiveIndeterminateProgresses().First() == key);
+            }
+            Assert.IsTrue(command.CanExecute(null));
+        }
+    }
+}
